83fc8f684cc16ffdbccbaf80041f9edc3428125a
Lilian Cisneros <liliancisneros29@gmail.com>
2016-05-17 10:30:03 -0600

    encontre error

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 
 namespace ContosoUniversity.Models
-{
+{//encontré error
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
